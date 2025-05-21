f4837280597ccb3d451fa7c28ea5a4c649bd0cb0
siann <happy2hyeon@gmail.com>
2025-05-21 13:15:05 +0900

    refactor: 해제 대상 없을 때는, confirm 띄우지 않도록 - 버튼 클릭 시 에러 발생 시 콘솔에 에러 로그 출력 - 알림 해제 확인 메시지에 채널 수 추가

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,27 +1,45 @@
 async function unsubscribe() {
-  const buttons = Array.from(
-    document.getElementsByClassName("btn_alarm type_on")
-  );
+  try {
+    const buttons = Array.from(
+      document.getElementsByClassName("btn_alarm type_on")
+    );
 
-  for (let i = 0; i < buttons.length; i++) {
-    (buttons[i] as HTMLElement).click();
+    console.debug("[알림 해제 프로그램] 대상 :", buttons.length);
 
-    await new Promise((resolve) => setTimeout(resolve, 300));
-
-    const funcButtons = document.getElementsByClassName("btn_func");
-    if (funcButtons.length >= 2) {
-      (funcButtons[1] as HTMLElement).click();
-      await new Promise((resolve) => setTimeout(resolve, 200));
-      (funcButtons[0] as HTMLElement).click();
+    if (!buttons.length) {
+      return;
     }
 
-    await new Promise((resolve) => setTimeout(resolve, 500));
+    if (
+      !confirm(
+        `모든 채널의 알림을 해제하시겠습니까? (채널 갯수: ${buttons.length})`
+      )
+    ) {
+      return;
+    }
+
+    for (let i = 0; i < buttons.length; i++) {
+      (buttons[i] as HTMLElement).click();
+
+      await new Promise((resolve) => setTimeout(resolve, 300));
+
+      const funcButtons = document.getElementsByClassName("btn_func");
+      if (funcButtons.length >= 2) {
+        (funcButtons[1] as HTMLElement).click();
+        await new Promise((resolve) => setTimeout(resolve, 200));
+        (funcButtons[0] as HTMLElement).click();
+      }
+
+      await new Promise((resolve) => setTimeout(resolve, 500));
+    }
+  } catch (error) {
+    console.error("에러 발생:", error);
   }
 }
 
 // 페이지가 로드되면 자동으로 실행
 if (window.location.href === "https://m.notify.naver.com/channel") {
-  if (confirm("모든 채널의 알림을 해제하시겠습니까?")) {
-    setTimeout(unsubscribe, 1000);
-  }
+  setTimeout(() => {
+    unsubscribe();
+  }, 1000);
 }
